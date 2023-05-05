3955a07d326941b8337648c2cabc47185d3191c6
Molnár Dávid <d.molnarka@hotmail.com>
2023-05-05 15:18:17 +0200

    Finish exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldestPerson, currentPerson) => {
+        const oldestAge = getAge(oldestPerson);
+        const currentAge = getAge(currentPerson);
+        return oldestAge > currentAge ? oldestPerson : currentPerson;
+    });
 };
+
+const getAge = function(person) {
+    return "yearOfDeath" in person ? person.yearOfDeath - person.yearOfBirth
+                                   : new Date().getFullYear() - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
