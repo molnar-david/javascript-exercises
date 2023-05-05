1083ef166abe0770babf1eea9e7c2edac1da53cb
Molnár Dávid <d.molnarka@hotmail.com>
2023-05-05 14:28:37 +0200

    Finish exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, value) => sum += value, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((prod, value) => prod *= value, 1);
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return x ** y;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n < 2) return 1;
+
+  let fact = 1;
+  for (let i=2; i<=n; i++) {
+    fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
