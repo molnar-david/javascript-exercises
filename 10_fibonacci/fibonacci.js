5afc75fe072c6adab5792124275345a085049cef
Molnár Dávid <d.molnarka@hotmail.com>
2023-05-05 14:57:21 +0200

    Finish exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n < 0) return "OOPS";
+    if (n === 0) return 0;
+    if (n <= 2) return 1;
 
+    let x = 1;
+    let y = 1;
+    for(i=3; i<=n; i++) {
+        let temp = x + y;
+        x = y;
+        y = temp;
+    }
+    return y;
 };
 
 // Do not edit below this line
